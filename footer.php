62aa94da6e0fd24e3f17a66a476339062ec17512
mfesri <mfrivera@stefanini.com>
2024-09-17 18:18:24 -0600

    add footer mobile

--- footer.php
+++ footer.php
@@ -1,6 +1,40 @@
-<footer>
-      <p>Copyright &copy; 2019</p>
-    </footer>
-    <?php wp_footer(); ?>
-  </body>
+<footer class="focostv-site-footer">
+  <div class="focostv-site-footer-logo">
+    <div class="focostv-footer-logo">
+      <a href="<?php echo esc_url(home_url('/')); ?>">
+        <img src="<?php echo esc_url(get_template_directory_uri() . '/images/focostv-logo-white.svg'); ?>"
+          alt="Logo de Focos TV">
+      </a>
+    </div>
+    <div class="focostv-site-footer-menu">
+      <?php
+      wp_nav_menu(array(
+        'theme_location' => 'footer_menu_principal',
+        'container' => false,
+        'menu_class' => 'focostv-footer-menu',
+        'fallback_cb' => false,
+      ));
+      ?>
+    </div>
+    <div class="focostv-site-footer-menu">
+      <?php
+      wp_nav_menu(array(
+        'theme_location' => 'footer_menu_secondary',
+        'container' => false,
+        'menu_class' => 'focostv-footer-menu-secondary',
+        'fallback_cb' => false,
+      ));
+      ?>
+    </div>
+    <div class="focostv-site-footer-copyright">
+      <p class="copyright-text">
+        © Copyright <?php echo date('Y'); ?>, <?php bloginfo('name'); ?> El Salvador, Todos los derechos
+        reservados.
+      </p>
+    </div>
+  </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+
 </html>
