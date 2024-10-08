8d3a979f47ef385df874a2afc044e5ef26c1f2b5
mfesri <mfrivera@stefanini.com>
2024-10-07 23:05:58 -0600

    podcasts mobile design

---
+++ components/perspectives-podcasts-posts.php
@@ -0,0 +1,40 @@
+<?php
+
+// show only most recent podcast publication
+$args = array(
+    'category_name' => 'podcasts',
+    'posts_per_page' => 1,
+    'orderby' => 'date',
+    'order' => 'DESC'
+);
+
+$podcasts_query = new WP_Query($args);
+
+if ($podcasts_query->have_posts()) {
+    while ($podcasts_query->have_posts()) {
+        $podcasts_query->the_post();
+        
+        echo '<div class="focostv-perspectives-podcast-spotify">';
+        echo the_content();
+        echo '</div>';
+    }
+    ?>
+    <div class="perspectives-all-podcasts-container">
+        <h6 class="focostv-perspectives-category">
+            Podcasts
+        </h6>
+        <h3 class="perspectives-all-podcasts-title">
+            Mira todos nuestros podcasts
+        </h3>
+        <?php
+        $permalink = 'https://open.spotify.com/show/2wVYsSbTxf1cvXlMmVmsYW?si=9710488683bd4a7a'; // url FOCOSTV PODCASTS
+        $custom_text = 'Ver playlist';
+        get_template_part('components/read-more-post', null, array('permalink' => $permalink, 'custom_text' => $custom_text));
+        ?>
+    </div>
+    <?php
+
+}
+
+
+?>
--- page-perspectivas.php
+++ page-perspectivas.php
@@ -3,7 +3,17 @@
 Template Name: Perspectivas Page
 */
 get_header(); ?>
-<main class="focotv-site-perspectivas">
-    <h1>FOCOS TV perspectivas</h1>
+<main class="focostv-site-page">
+    <div class="focostv-go-home-container">
+        <i class="fa-solid fa-chevron-left"></i>
+        <a href="<?php echo esc_url(home_url('/')); ?>">Inicio</a>
+    </div>
+    <h3 class="focostv-sections-title focostv-page-title"><a>Perspectivas</a>
+    </h3>
+    <section class="perspectives-podcasts">
+        <?php
+        get_template_part('components/perspectives-podcasts-posts');
+        ?>
+    </section>
 </main>
 <?php get_footer(); ?>
