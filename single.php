37335ae498837cb8528b8d0110f963a1dbbad90a
mfesri <mfrivera@stefanini.com>
2024-10-13 12:09:50 -0600

    desktop single styles

--- single.php
+++ single.php
@@ -27,35 +27,37 @@ if (have_posts()):
                     </div>
                 <?php endif; ?>
                 <h1 class="focostv-post-title"><?php the_title(); ?></h1>
-                <?php
-                $opinion_author = get_field('autor_opinion');
-                $opinion_avatar_author = get_field('imagen_autor_opinion');
-                if ($opinion_author && $opinion_avatar_author) {
+                <div class="focostv-post-header-information">
+                    <?php
+                    $opinion_author = get_field('autor_opinion');
+                    $opinion_avatar_author = get_field('imagen_autor_opinion');
+                    if ($opinion_author && $opinion_avatar_author) {
 
+                        ?>
+                        <div class="focostv-perspectives-post-author">
+                            <img src="<?php echo esc_url($opinion_avatar_author['url']); ?>"
+                                alt="<?php echo esc_attr($opinion_author); ?>" width="60" height="60"
+                                class="focostv-perspectives-post-author-avatar">
+                            <span class="focostv-perspectives-post-by-author">Por</span>
+                            <span class="focostv-perspectives-post-author-name"><?php echo esc_html($opinion_author); ?></span>
+                        </div>
+                        <?php
+                    } else {
+                        ?>
+                        <div class="focostv-perspectives-post-author">
+                            <?php echo get_avatar(get_the_author_meta('ID'), 60); ?>
+                            <span class="focostv-perspectives-post-by-author">Por&nbsp;</span>
+                            <span class="focostv-perspectives-post-author-name"><?php echo esc_html(get_the_author()); ?></span>
+                        </div>
+                        <?php
+                    }
+                    $post_location = get_field('ubicacion_publicacion');
                     ?>
-                    <div class="focostv-perspectives-post-author">
-                        <img src="<?php echo esc_url($opinion_avatar_author['url']); ?>"
-                            alt="<?php echo esc_attr($opinion_author); ?>" width="60" height="60"
-                            class="focostv-perspectives-post-author-avatar">
-                        <span class="focostv-perspectives-post-by-author">Por</span>
-                        <span class="focostv-perspectives-post-author-name"><?php echo esc_html($opinion_author); ?></span>
+                    <div class="focostv-post-location-date-container">
+                        <?php if ($post_location): ?><span class="focostv-post-location"><?php echo $post_location; ?>,</span>
+                        <?php endif; ?>
+                        <span class="focostv-post-date"><?php echo get_the_date('d \d\e F \d\e Y'); ?></span>
                     </div>
-                    <?php
-                } else {
-                    ?>
-                    <div class="focostv-perspectives-post-author">
-                        <?php echo get_avatar(get_the_author_meta('ID'), 60); ?>
-                        <span class="focostv-perspectives-post-by-author">Por&nbsp;</span>
-                        <span class="focostv-perspectives-post-author-name"><?php echo esc_html(get_the_author()); ?></span>
-                    </div>
-                    <?php
-                }
-                $post_location = get_field('ubicacion_publicacion');
-                ?>
-                <div class="focostv-post-location-date-container">
-                    <?php if ($post_location): ?><span class="focostv-post-location"><?php echo $post_location; ?>,</span>
-                    <?php endif; ?>
-                    <span class="focostv-post-date"><?php echo get_the_date('d \d\e F \d\e Y'); ?></span>
                 </div>
             </header>
             <?php
