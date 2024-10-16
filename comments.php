1c11fe7121f5d425adecadb8214711b131f184dc
mfesri <mfrivera@stefanini.com>
2024-10-15 20:37:49 -0600

    mobile comments section

--- comments.php
+++ comments.php
@@ -47,16 +47,38 @@ if (post_password_required()) {
         </ol>
 
         <?php the_comments_navigation(); ?>
-
+    <?php else: ?>
+        <div class="focostv-no-comments-container">
+            <h3 class="focostv-no-comments-post-title">¡S&eacute; el primero en comentar!</h3>
+        </div>
     <?php endif; ?>
-
     <?php
     // If comments are closed and there are comments, let's leave a little note, shall we?
     if (!comments_open() && get_comments_number() && post_type_supports(get_post_type(), 'comments')):
         ?>
         <p class="no-comments"><?php _e('Comments are closed.', 'textdomain'); ?></p>
+
+    <?php else: ?>
+        <div class="focostv-post-comment-form">
+            <?php
+            $comment_form_args = array(
+                'title_reply' => '',
+                'label_submit' => 'Comentar',
+                'class_submit' => 'focostv-comment-submit-button',
+                'comment_notes_before' => '<p class="focostv-comment-note">Al comentar estas aceptado las <span>normas de convivencia</span> de la plataforma</p>',
+                'comment_notes_after' => '',
+                'fields' => array(
+                    'author' => '<p class="comment-form-author focostv-comment-form-author"><input id="author" placeholder="Nombre" name="author" type="text" value="' . esc_attr($commenter['comment_author']) . '" required/></p>',
+                    'email' => '<p class="comment-form-email focostv-comment-form-email"><input id="email" placeholder="Email" name="email" type="email" value="' . esc_attr($commenter['comment_author_email']) . '" required/></p>',
+                ),
+                'comment_field' => '<p class="comment-form-comment"><textarea id="comment" name="comment" class="focostv-comment-textarea" placeholder="Únete a la conversación..." rows="8" aria-required="true" required></textarea></p>',
+            );
+
+            comment_form($comment_form_args);
+            ?>
+        </div>
     <?php endif; ?>
 
-    <?php comment_form(); ?>
+
 
 </div>
