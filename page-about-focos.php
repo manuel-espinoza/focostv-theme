69319f767ac1a414f7befe95524007db1b57cbe6
mfesri <mfrivera@stefanini.com>
2024-10-18 14:05:32 -0600

    page sobre focos

--- page-about-focos.php
+++ page-about-focos.php
@@ -3,7 +3,24 @@
 Template Name: Sobre Focos Page
 */
 get_header(); ?>
-<main class="focotv-site-about">
-    <h1>FOCOS TV About</h1>
+<main class="focostv-site-page">
+    <div class="focostv-go-home-container">
+        <i class="fa-solid fa-chevron-left"></i>
+        <a href="<?php echo esc_url(home_url('/')); ?>">Inicio</a>
+    </div>
+    <?php
+    while (have_posts()):
+        the_post(); ?>
+
+        <h1 class="focostv-sections-title">
+            <?php echo the_title(); ?>
+        </h1>
+
+        <section class="focostv-about-content">
+            <?php echo the_content(); ?>
+        </section>
+        <?php
+    endwhile;
+    ?>
 </main>
 <?php get_footer(); ?>
