7b4d89d7d8ff1ed8fd560e5de700b31135efec6a
mfesri <mfrivera@stefanini.com>
2024-10-17 21:32:43 -0600

    desktop documentaries posts

--- components/documentaries-single-post.php
+++ components/documentaries-single-post.php
@@ -29,41 +29,42 @@ if (!empty($categories)) {
     <div class="focostv-post-content focostv-documentaries-post-content">
         <?php the_content(); ?>
     </div>
-
-    <div class="focostv-post-author focostv-documentaries-post-authors">
-        <h3 class="focostv-documentaries-posts-authors-title">Cr&eacute;ditos</h3>
-        <?php
-        $opinion_author = get_field('autor_opinion');
-        $opinion_avatar_author = get_field('imagen_autor_opinion');
-        $opinion_author_description = get_field('descripcion_autor');
-        if ($opinion_author && $opinion_avatar_author) {
-
-            ?>
-            <div class="focostv-perspectives-post-author">
-                <img src="<?php echo esc_url($opinion_avatar_author['url']); ?>"
-                    alt="<?php echo esc_attr($opinion_author); ?>" width="60" height="60"
-                    class="focostv-perspectives-post-author-avatar">
-                <span class="focostv-perspectives-post-author-name"><?php echo esc_html($opinion_author); ?></span>
-            </div>
-            <div class="focostv-post-author-description">
-                <p><?php echo $opinion_author_description; ?></p>
-            </div>
+    <section class="documentaries-post-footer">
+        <div class="focostv-post-author focostv-documentaries-post-authors">
+            <h3 class="focostv-documentaries-posts-authors-title">Cr&eacute;ditos</h3>
             <?php
-        } else {
-            ?>
-            <div class="focostv-perspectives-post-author">
-                <?php echo get_avatar(get_the_author_meta('ID'), 60); ?>
-                <span class="focostv-perspectives-post-author-name"><?php echo esc_html(get_the_author()); ?></span>
-            </div>
-            <div class="focostv-post-author-description">
-                <p><?php echo esc_html(get_the_author_meta('description')); ?></p>
-            </div>
-            <?php
-        }
-        ?>
-    </div>
+            $opinion_author = get_field('autor_opinion');
+            $opinion_avatar_author = get_field('imagen_autor_opinion');
+            $opinion_author_description = get_field('descripcion_autor');
+            if ($opinion_author && $opinion_avatar_author) {
 
-    <div class="focostv-coffee-donations-container">
-        <?php get_template_part('components/coffee-donations') ?>
-    </div>
+                ?>
+                <div class="focostv-perspectives-post-author">
+                    <img src="<?php echo esc_url($opinion_avatar_author['url']); ?>"
+                        alt="<?php echo esc_attr($opinion_author); ?>" width="60" height="60"
+                        class="focostv-perspectives-post-author-avatar">
+                    <span class="focostv-perspectives-post-author-name"><?php echo esc_html($opinion_author); ?></span>
+                </div>
+                <div class="focostv-post-author-description">
+                    <p><?php echo $opinion_author_description; ?></p>
+                </div>
+                <?php
+            } else {
+                ?>
+                <div class="focostv-perspectives-post-author">
+                    <?php echo get_avatar(get_the_author_meta('ID'), 60); ?>
+                    <span class="focostv-perspectives-post-author-name"><?php echo esc_html(get_the_author()); ?></span>
+                </div>
+                <div class="focostv-post-author-description">
+                    <p><?php echo esc_html(get_the_author_meta('description')); ?></p>
+                </div>
+                <?php
+            }
+            ?>
+        </div>
+
+        <div class="focostv-coffee-donations-container">
+            <?php get_template_part('components/coffee-donations') ?>
+        </div>
+    </section>
 </article>
