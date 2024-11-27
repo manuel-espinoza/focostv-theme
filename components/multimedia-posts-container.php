0f0a5171ea2543c78bc8137976d2846b00ad98c1
mfesri <mfrivera@stefanini.com>
2024-11-27 17:53:22 -0600

    multimedia desktop design completed

--- components/multimedia-posts-container.php
+++ components/multimedia-posts-container.php
@@ -2,11 +2,15 @@
     <h3 class="focostv-sections-title focostv-header-documentaries focostv-page-title">
         <a>Multimedia</a>
     </h3>
-    <?php get_template_part('components/multimedia-video-posts') ?>
-    <section class="focostv-main-advertisement focostv-ad-multimedia-page">
-        <div id="dynamic-advertisement">
-            <?php echo do_shortcode('[focostv_advertising group="mobile_page"]'); ?>
+    <div class="focostv-multimedia-container">
+        <div class="multimedia-video-posts-container">
+            <?php get_template_part('components/multimedia-video-posts') ?>
         </div>
-    </section>
-    <?php get_template_part('components/multimedia-podcasts-posts') ?>
+        <section class="focostv-main-advertisement focostv-ad-multimedia-page">
+            <div id="dynamic-advertisement">
+                <?php echo do_shortcode('[focostv_advertising group="mobile_page"]'); ?>
+            </div>
+        </section>
+        <?php get_template_part('components/multimedia-podcasts-posts') ?>
+    </div>
 </section>
