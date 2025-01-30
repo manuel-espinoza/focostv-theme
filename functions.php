bfdc0b327d02c9063437d6a14a8ec55e598398fa
mfesri <mfrivera@stefanini.com>
2025-01-30 13:52:53 -0600

    fix: adjust small image size to maintain aspect ratio

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 function focostvtheme_setup_menus()
 {
     add_theme_support('post-thumbnails');
-    add_image_size('small', 360, 9999);
+    add_image_size('small', 360, 360);
     add_image_size('medium', 768, 9999);
     add_image_size('large', 1024, 9999);
 
